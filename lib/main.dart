4e2730f82ca9e799969746ef52810fd831d31b38
DargaHasanBasri <85760132+DargaHasanBasri@users.noreply.github.com>
2024-10-04 23:25:13 +0300

    Add easy localization

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,31 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:word_prime/routes/app_routes.dart';
 import 'package:word_prime/routes/routes.dart';
 import 'package:word_prime/services/service_locator.dart';
+import 'package:word_prime/utils/app_locale_constants.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await EasyLocalization.ensureInitialized();
   SystemChrome.setSystemUIOverlayStyle(
     const SystemUiOverlayStyle(
       statusBarColor: Colors.transparent,
     ),
   );
   await setupLocator();
-  runApp(const MyApp());
+  runApp(
+    EasyLocalization(
+      supportedLocales: const [
+        AppLocaleConstants.TR_LOCALE,
+        AppLocaleConstants.EN_LOCALE,
+      ],
+      path: AppLocaleConstants.LANG_PATH,
+      fallbackLocale: AppLocaleConstants.EN_LOCALE,
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -22,6 +35,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
+      localizationsDelegates: context.localizationDelegates,
+      supportedLocales: context.supportedLocales,
+      locale: context.locale,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
---
+++ lib/utils/app_locale_constants.dart
@@ -0,0 +1,9 @@
+import 'dart:ui';
+
+final class AppLocaleConstants {
+  AppLocaleConstants._();
+
+  static const TR_LOCALE = Locale('tr', 'TR');
+  static const EN_LOCALE = Locale('en', 'US');
+  static const LANG_PATH = 'assets/langs';
+}
