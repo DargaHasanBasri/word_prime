a7bd8a7d3ec6a46f564ba7228feac9274202cae3
DargaHasanBasri <85760132+DargaHasanBasri@users.noreply.github.com>
2024-10-06 21:19:56 +0300

    Update login page UI and check email-password

--- lib/ui/pages/login/login_page.dart
+++ lib/ui/pages/login/login_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:word_prime/base/base_stateful_state.dart';
 import 'package:word_prime/generated/locale_keys.g.dart';
+import 'package:word_prime/ui/pages/login/components/or_text_container.dart';
 import 'package:word_prime/ui/pages/login/login_view_model.dart';
 import 'package:word_prime/ui/widgets/custom_button.dart';
 import 'package:word_prime/ui/widgets/custom_text_form_field.dart';
@@ -17,8 +19,16 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends BaseStatefulState<LoginPage> {
+  late final LoginViewModel _vm;
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
+
+  @override
+  void initState() {
+    _vm = Provider.of<LoginViewModel>(context, listen: false);
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,174 +38,183 @@ class _LoginPageState extends BaseStatefulState<LoginPage> {
   }
 
   Widget _buildBody() {
-    LoginViewModel _vm = LoginViewModel();
-    return SingleChildScrollView(
-      child: ConstrainedBox(
-        constraints: BoxConstraints(
-          minHeight: MediaQuery.of(context).size.height,
-        ),
-        child: IntrinsicHeight(
-          child: Padding(
-            padding: AppPaddings.appPaddingHorizontal,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(top: 70, bottom: 8),
-                  child: Text(
-                    LocaleKeys.loginPage_welcomeTitle.locale,
-                    style: TextStyle(
-                      color: AppColors.mirage,
-                      fontSize: 28,
-                      fontWeight: FontWeight.w700,
+    return SafeArea(
+      child: Padding(
+        padding: AppPaddings.appPaddingHorizontal,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Expanded(
+              child: SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(top: 60),
+                      child: _titleSubtitleText(),
                     ),
-                  ),
-                ),
-                Text(
-                  LocaleKeys.loginPage_welcomeSubTitle.locale,
-                  style: TextStyle(
-                    color: AppColors.paleSky,
-                    fontSize: 16,
-                    fontWeight: FontWeight.w400,
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 36, bottom: 16),
-                  child: CustomTextFormField(
-                    controller: _emailController,
-                    isPrefixIcon: true,
-                    prefixIconAddress: AppAssets.icSmsPath,
-                    hintText: LocaleKeys.emailHintText.locale,
-                    inputType: TextInputType.emailAddress,
-                  ),
-                ),
-                ValueListenableBuilder(
-                  valueListenable: _vm.isActive,
-                  builder: (_, __, ___) {
-                    return CustomTextFormField(
-                      controller: _passwordController,
-                      isPrefixIcon: true,
-                      isSuffixIcon: true,
-                      isHaveObscure: _vm.isActive.value,
-                      prefixIconAddress: AppAssets.icLockPath,
-                      suffixIconAddress: _vm.isActive.value == true
-                          ? AppAssets.icCloseEyePath
-                          : AppAssets.icLockPath,
-                      hintText: LocaleKeys.passwordHintText.locale,
-                      textInputAction: TextInputAction.done,
-                      onPressSuffixIcon: () {
-                        _vm.isActive.value = !_vm.isActive.value;
+                    ValueListenableBuilder(
+                        valueListenable: _vm.emailInput,
+                        builder: (_, __, ___) {
+                          return Padding(
+                            padding: const EdgeInsets.only(top: 36, bottom: 16),
+                            child: CustomTextFormField(
+                              controller: _emailController,
+                              isPrefixIcon: true,
+                              prefixIconAddress: AppAssets.icSmsPath,
+                              hintText: LocaleKeys.emailHintText.locale,
+                              inputType: TextInputType.emailAddress,
+                              onChanged: (String text) {
+                                _vm.emailInput.value = text;
+                              },
+                            ),
+                          );
+                        }),
+                    ValueListenableBuilder(
+                      valueListenable: _vm.passwordInput,
+                      builder: (_, __, ___) {
+                        return ValueListenableBuilder(
+                          valueListenable: _vm.isActive,
+                          builder: (_, __, ___) {
+                            return CustomTextFormField(
+                              controller: _passwordController,
+                              isPrefixIcon: true,
+                              isSuffixIcon: true,
+                              isHaveObscure: _vm.isActive.value,
+                              prefixIconAddress: AppAssets.icLockPath,
+                              suffixIconAddress: _vm.isActive.value == true
+                                  ? AppAssets.icCloseEyePath
+                                  : AppAssets.icLockPath,
+                              hintText: LocaleKeys.passwordHintText.locale,
+                              textInputAction: TextInputAction.done,
+                              onPressSuffixIcon: () {
+                                _vm.isActive.value = !_vm.isActive.value;
+                              },
+                              onChanged: (String text) {
+                                _vm.passwordInput.value = text;
+                              },
+                            );
+                          },
+                        );
                       },
-                    );
-                  },
-                ),
-                const SizedBox(height: 16),
-                Text(
-                  LocaleKeys.loginPage_forgotPassword.locale,
-                  style: TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.w700,
-                    color: AppColors.cornflowerBlue,
-                  ),
-                  textAlign: TextAlign.end,
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 24),
-                  child: CustomButton(
-                    title: LocaleKeys.signIn.locale,
-                    onClick: () {},
-                  ),
-                ),
-                _orTextContainer(),
-                Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 16),
-                  child: CustomButton(
-                    title: LocaleKeys.loginPage_withApple.locale,
-                    titleColor: AppColors.mirage,
-                    backgroundColor: AppColors.white,
-                    borderColor: AppColors.softPeach,
-                    borderRadius: 12,
-                    isIcon: true,
-                    iconLogoAddress: AppAssets.icApplePath,
-                    onClick: () {},
-                  ),
-                ),
-                CustomButton(
-                  title: LocaleKeys.loginPage_withGoogle.locale,
-                  titleColor: AppColors.mirage,
-                  backgroundColor: AppColors.white,
-                  borderColor: AppColors.softPeach,
-                  borderRadius: 12,
-                  isIcon: true,
-                  iconLogoAddress: AppAssets.icGooglePath,
-                  onClick: () {},
-                ),
-                const Spacer(),
-                Padding(
-                  padding: const EdgeInsets.only(bottom: 20),
-                  child: Center(
-                    child: RichText(
-                      text: TextSpan(
-                        text: LocaleKeys.loginPage_isSignUpMsg.locale,
-                        style: TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.w500,
-                          color: AppColors.mirage,
-                        ),
-                        children: <TextSpan>[
-                          TextSpan(
-                              text: ' ${LocaleKeys.signUp.locale}',
-                              style: TextStyle(
-                                fontSize: 16,
-                                fontWeight: FontWeight.w700,
-                                color: AppColors.cornflowerBlue,
-                              )),
-                        ],
+                    ),
+                    const SizedBox(height: 16),
+                    Text(
+                      LocaleKeys.loginPage_forgotPassword.locale,
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w700,
+                        color: AppColors.cornflowerBlue,
+                      ),
+                      textAlign: TextAlign.end,
+                    ),
+                    ValueListenableBuilder(
+                      valueListenable: _vm.emailInput,
+                      builder: (_, __, ___) {
+                        return ValueListenableBuilder(
+                          valueListenable: _vm.passwordInput,
+                          builder: (_, __, ___) {
+                            return Padding(
+                              padding: const EdgeInsets.symmetric(vertical: 24),
+                              child: CustomButton(
+                                title: LocaleKeys.signIn.locale,
+                                backgroundColor: _vm.isEmptyInputText()
+                                    ? AppColors.cornflowerBlue.withOpacity(0.4)
+                                    : AppColors.cornflowerBlue,
+                                onClick: () {},
+                              ),
+                            );
+                          },
+                        );
+                      },
+                    ),
+                    const OrTextContainer(),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 16),
+                      child: CustomButton(
+                        title: LocaleKeys.loginPage_withApple.locale,
+                        titleColor: AppColors.mirage,
+                        backgroundColor: AppColors.white,
+                        borderColor: AppColors.softPeach,
+                        borderRadius: 12,
+                        isIcon: true,
+                        iconLogoAddress: AppAssets.icApplePath,
+                        onClick: () {},
                       ),
                     ),
-                  ),
+                    CustomButton(
+                      title: LocaleKeys.loginPage_withGoogle.locale,
+                      titleColor: AppColors.mirage,
+                      backgroundColor: AppColors.white,
+                      borderColor: AppColors.softPeach,
+                      borderRadius: 12,
+                      isIcon: true,
+                      iconLogoAddress: AppAssets.icGooglePath,
+                      onClick: () {},
+                    ),
+                  ],
                 ),
-              ],
+              ),
             ),
-          ),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 20),
+              child: Center(
+                child: _richTextSignUp(),
+              ),
+            ),
+          ],
         ),
       ),
     );
   }
 
-  Widget _orTextContainer() {
-    return Row(
+  Widget _titleSubtitleText() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Expanded(
-          child: Container(
-            height: 2,
-            decoration: BoxDecoration(
-              color: AppColors.softPeach,
-              borderRadius: BorderRadius.circular(6),
-            ),
-          ),
-        ),
         Padding(
-          padding: AppPaddings.paddingMediumHorizontal,
+          padding: const EdgeInsets.only(bottom: 8),
           child: Text(
-            LocaleKeys.or.locale,
+            LocaleKeys.loginPage_welcomeTitle.locale,
             style: TextStyle(
-              fontSize: 14,
-              fontWeight: FontWeight.w500,
-              color: AppColors.santaGrey,
+              color: AppColors.mirage,
+              fontSize: 28,
+              fontWeight: FontWeight.w700,
             ),
           ),
         ),
-        Expanded(
-          child: Container(
-            height: 2,
-            decoration: BoxDecoration(
-              color: AppColors.softPeach,
-              borderRadius: BorderRadius.circular(6),
-            ),
+        Text(
+          LocaleKeys.loginPage_welcomeSubTitle.locale,
+          style: TextStyle(
+            color: AppColors.paleSky,
+            fontSize: 16,
+            fontWeight: FontWeight.w400,
           ),
         ),
       ],
+    );
+  }
+
+  Widget _richTextSignUp() {
+    return RichText(
+      text: TextSpan(
+        text: LocaleKeys.loginPage_isSignUpMsg.locale,
+        style: TextStyle(
+          fontSize: 16,
+          fontWeight: FontWeight.w500,
+          color: AppColors.mirage,
+        ),
+        children: <TextSpan>[
+          TextSpan(
+            text: ' ${LocaleKeys.signUp.locale}',
+            style: TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.w700,
+              color: AppColors.cornflowerBlue,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
--- lib/ui/pages/login/login_view_model.dart
+++ lib/ui/pages/login/login_view_model.dart
@@ -5,4 +5,15 @@ class LoginViewModel extends BaseViewModel {
   /// Variable where I keep the change of the value of the isHaveObscure
   /// property in the text form field
   ValueNotifier<bool> isActive = ValueNotifier(true);
+
+  /// Variables in which I store the changes of email and password
+  /// values in the text form field
+  ValueNotifier<String> emailInput = ValueNotifier('');
+  ValueNotifier<String> passwordInput = ValueNotifier('');
+
+  /// Check whether the contents of the email and password variables are empty
+  bool isEmptyInputText() {
+    if (emailInput.value.isEmpty || passwordInput.value.isEmpty) return true;
+    return false;
+  }
 }
