0471b1919d67719b252261e839fd2a8eb44c2e61
DargaHasanBasri <85760132+DargaHasanBasri@users.noreply.github.com>
2024-10-12 20:16:24 +0300

    Update app icon and name

--- lib/main.dart
+++ lib/main.dart
@@ -31,7 +31,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: AppLocaleConstants.APP_NAME,
       localizationsDelegates: context.localizationDelegates,
       supportedLocales: context.supportedLocales,
       locale: context.locale,
--- lib/utils/app_locale_constants.dart
+++ lib/utils/app_locale_constants.dart
@@ -3,6 +3,8 @@ import 'dart:ui';
 final class AppLocaleConstants {
   AppLocaleConstants._();
 
+  static const APP_NAME = 'Word Prime';
+
   static const TR_LOCALE = Locale('tr', 'TR');
   static const EN_LOCALE = Locale('en', 'US');
   static const LANG_PATH = 'assets/langs';
